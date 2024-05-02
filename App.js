fd893bf6f854da758e595b60782c3547fea6e62c
Daniel <wilprotom@email.com>
2024-05-01 23:34:24 -0300

    Roginer

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Text, StyleSheet, View, Image,FlatList} from 'react-native';
+import Header from './components/Header';
+import Card from './components/Card';
+import ROTINAS from './data';
+
+export default function App() {
+  return (
+    <View style={estilo.container}>
+      <Header></Header>
+     <View style = {{width:'100%'}}>
+      <FlatList 
+      data = {ROTINAS}
+      keyExtractor = {(item)=>item.id}
+      renderItem = {({item}) => (
+
+        <Card 
+        titulo = {item.titulo}
+        descricao = {item.Descricao}
+        porcentagem = {item.porcentagem}
+        imagem ={item.imagem}
+        
+        />
+
+      )}
+      
+      
+      /> 
+      </View>     
+
+
+
+
+
+
+  
+      
+     
+      
+    </View>
+  );
+}
+const estilo = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#62e384',
+    borderColor: '#90E0EF',
+    paddingHorizontal: 20,
+  }
+});
---
+++ data/index.js
@@ -0,0 +1,46 @@
+let DATA =  [
+    {
+    id:'01',
+    porcentagem :'30%', 
+    titulo : 'Dormir',
+    imagem :'https://th.bing.com/th/id/R.7292f426df8369a23dd92dc4fba82835?rik=yEh7CKJ2CpI9cQ&pid=ImgRaw&r=0',
+     Descricao :'Durmo'  
+    },
+    {
+     id:'02',
+
+     porcentagem :'25%', 
+    titulo : 'Estudar',
+     imagem :'https://img.freepik.com/vetores-gratis/pilha-de-livros-de-design-plano-desenhado-a-mao_23-2149334862.jpg',
+     Descricao :'Estudo' 
+    },
+    {
+        id:'03',
+        porcentagem :'15%', 
+        titulo : 'jogar Video Game',
+        imagem :'https://openclipart.org/image/2400px/svg_to_png/192404/Game-Icon.png',
+        Descricao :'Perco no fifa'  
+    },
+    {
+        id:'04',
+        porcentagem :'20%', 
+        titulo : 'Arrumar a casa',
+        imagem :'https://toolbx-order-images.s3.amazonaws.com/184909bb-6258-42de-9c75-6bae43591932.jpg',
+        Descricao :'Arrumo minha casa'  
+    },
+
+    {
+        id:'05',
+        porcentagem :'10%', 
+        titulo : 'Ler',
+        imagem :'https://static.vecteezy.com/system/resources/previews/000/352/087/original/vector-reading-icon.jpg',
+        Descricao :'Leio livros'  
+    }
+
+
+
+
+
+]
+
+export default DATA;
